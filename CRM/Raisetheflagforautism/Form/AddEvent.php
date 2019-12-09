5d2a9dcf65924d81864b4a9689f4684377dc85b9
Monish Deb <deb.monish@gmail.com>
2019-12-09 23:18:00 +0530

    add event description field

--- CRM/Raisetheflagforautism/Form/AddEvent.php
+++ CRM/Raisetheflagforautism/Form/AddEvent.php
@@ -133,7 +133,7 @@ class CRM_Raisetheflagforautism_Form_AddEvent extends CRM_Core_Form {
       ],
       'event_description' => [
         'title' => ts('Event Description'),
-        'type' => 'text',
+        'type' => 'textarea',
         'required' => FALSE,
       ],
       'location' => [
